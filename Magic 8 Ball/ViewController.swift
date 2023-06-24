5b6901ab8c2dff4c2bb8068bbfb90ae69df0718a
Mark Faust <markfaust123@gmail.com>
2023-06-24 11:04:25 -0400

    addition of shaking functionality to the Magic 8 Ball

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,24 +12,46 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var magicBall: UIImageView!
     
-    let ballArray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4")]
+    let ballArray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5")]
     
-    var ballNumber = 3
+    var ballNumber = 2
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.becomeFirstResponder() // To get shake gesture
         magicBall.isHidden = true
     }
     
+    override var canBecomeFirstResponder: Bool {
+        get {
+            return true
+        }
+    }
+    
+    // Enable detection of shake motion
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            print("Shake!!!")
+            
+            // Make sure ball is unhidden
+            magicBall.isHidden = false
+            print("Ball Number: \(ballNumber)")
+            magicBall.image = ballArray[ballNumber]
+            
+            // Find new random number for next ask
+            ballNumber = Int.random(in:0...4)
+        }
+    }
+    
     @IBAction func askButtonPressed(_ sender: UIButton) {
+        // Make sure ball is unhidden
         magicBall.isHidden = false
-        
         // magicBall.image = ballArray.randomElement()
-        
+        print("Ball Number: \(ballNumber)")
         magicBall.image = ballArray[ballNumber]
         
+        // Find new random number for next ask
         ballNumber = Int.random(in:0...4)
-        
     }
     
     
